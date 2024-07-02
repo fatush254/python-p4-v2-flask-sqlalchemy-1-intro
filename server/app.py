bc5bee27d0ac6278d239c9fedf3e123f3c314adf
fatush254 <fatuma.galgalo@student.moringaschool.com>
2024-07-02 12:53:23 +0300

    complete

--- server/app.py
+++ server/app.py
@@ -7,6 +7,22 @@ from models import db
 
 # create a Flask application instance
 app = Flask(__name__)
+# server/app.py
+
+from flask import Flask
+from flask_migrate import Migrate
+
+from models import db
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+migrate = Migrate(app, db)
+db.init_app(app)
+
+if __name__ == '__main__':
+    app.run(port=5555, debug=True)
 
 # configure the database connection to the local file app.db
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
--- server/models.py
+++ server/models.py
@@ -5,7 +5,21 @@ from sqlalchemy import MetaData
 metadata = MetaData()
 
 # create the Flask SQLAlchemy extension
+db = SQLAlchemy(metadata=metadata)# server/models.py
+
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import MetaData
+
+metadata = MetaData()
 db = SQLAlchemy(metadata=metadata)
+
+class Pet(db.Model):
+    __tablename__ = 'pets'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
+    species = db.Column(db.String)
+
 
 # define a model class by inheriting from db.Model.
 
